9bcf4920a0d28c5346443185b131d6d11bdedfb7
Chris Wood <chriswood.ca@gmail.com>
2022-11-18 14:07:19 -0800

    remove tomcat 7

--- 7-alpine/50-copy-config.sh
+++
@@ -1,8 +0,0 @@
-config_path=${CONFIG_PATH:-/config}
-dest_path=${TOMCAT_CONFIG_PATH:-/usr/local/tomcat/conf}
-
-if [ -d $config_path ]; then
-    for f in $(find -L ${config_path} -maxdepth 1 -type f);do
-      cp -fv $f ${dest_path}/
-    done
-fi
